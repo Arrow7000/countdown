230cd74571417df7de86f586d00df6de70ca0ca4
Aron Adler <aronadler@hotmail.com>
2017-04-20 21:19:34 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,82 @@
+const units = ['days', 'hours', 'minutes', 'seconds'];
+
+const countdown = document.getElementById('countdown');
+
+// const pathString = window.location.pathname;
+// const pathString = window.location.hash.split('#')[1];
+
+countdown.appendChild(createTimeElem(14, 'minutes'));
+
+
+setTimer();
+setInterval(setTimer, 1000);
+
+
+
+/**
+ * Dirty function
+ */
+
+function setTimer() {
+    const pathString = window.location.hash.split('#')[1];
+
+    let times;
+    try {
+        times = getTimeObject(pathString);
+    } catch (error) {
+        console.error(error);
+        const message = `Couldn't parse time string :(`;
+        const errElem = createElem('p', message, 'error__message');
+
+        countdown.innerHTML = '';
+        countdown.appendChild(errElem);
+        return;
+    }
+    const elems = units.map(unit => {
+        return createTimeElem(times[unit], unit);
+    });
+
+    countdown.innerHTML = '';
+    elems.forEach(elem => {
+        countdown.appendChild(elem);
+    });
+}
+
+
+/**
+ * Semi-pure functions
+ */
+
+function getTimeObject(pathString) {
+    const now = moment();
+    const target = moment(pathString);
+    if (!target.isValid()) {
+        throw new Error('Error parsing path string');
+    }
+    const diff = target.diff(now);
+    const timeLeft = moment.duration(diff);
+
+    const days = Math.floor(timeLeft.asDays());
+    const hours = timeLeft.hours();
+    const minutes = timeLeft.minutes();
+    const seconds = timeLeft.seconds();
+    return { days, hours, minutes, seconds };
+}
+
+function createElem(tag, innerHTML, className) {
+    const elem = document.createElement(tag);
+    elem.innerHTML = innerHTML !== undefined ? innerHTML : '';
+    elem.setAttribute('class', className || '');
+    return elem;
+}
+
+function createTimeElem(num, unit) {
+    const container = createElem('div', null, 'countdown__item');
+    const numChild = createElem('p', num, 'countdown__number');
+    const unitChild = createElem('p', unit, 'countdown__unit');
+
+    container.appendChild(numChild);
+    container.appendChild(unitChild);
+
+    return container;
+}
