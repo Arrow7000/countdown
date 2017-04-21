23817156b077124dbc2f0dc6e8c9b77308c92a23
Aron Adler <aron@smartowldesign.com>
2017-04-21 02:22:04 +0100

    Use node as server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const compression = require('compression')
 
+const app = express();
+app.use(compression());
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/:datetime?', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+const port = process.env.PORT || 3000;
+console.log('Running app on port', port);
+app.listen(port);
